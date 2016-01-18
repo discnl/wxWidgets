a1cbfe4560dbe930f526f2bb6d56908230a2a9d3
Tobias Taschner <TcT2k@users.noreply.github.com>
2016-01-18 14:30:54 +0100

    Update outdated year references to 2016.
    Use 2016 instead of 2015, mostly in version info files.
    
    (cherry picked from commit 1f0a82b6a778a9894418755785583d822d50b859)

--- src/common/utilscmn.cpp
+++ src/common/utilscmn.cpp
@@ -1430,7 +1430,7 @@ wxVersionInfo wxGetLibraryVersionInfo()
                          wxMINOR_VERSION,
                          wxRELEASE_NUMBER,
                          msg,
-                         wxS("Copyright (c) 1995-2013 wxWidgets team"));
+                         wxS("Copyright (c) 1995-2016 wxWidgets team"));
 }
 
 void wxInfoMessageBox(wxWindow* parent)
